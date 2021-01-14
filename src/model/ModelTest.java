bcf454f58d148e0a7a5559075124fe748254a61b
JRoemisch <johannes.roemisch@student.fhws.de>
2021-01-14 16:32:46 +0100

    Update ModelTest.java

--- src/model/ModelTest.java
+++ src/model/ModelTest.java
@@ -1,13 +1,21 @@
 package model;
 
+import java.util.List;
+
+import zyklusFinder.AllCyclesInDirectedGraphJohnson;
+
 public class ModelTest {
 	public static void main(String[] args) {
 //		Graph g = Graph.fromFile("graph1Test.txt");
-		Graph g = Graph.fromFile("GraphOhneZyklus.txt");
+		Graph g = Graph.fromFile("GraphDreiZyklen.txt");
 		System.out.println("Graph dargestellt:");
 		System.out.println(g);
 
-		System.out.println("SubGraph dargestellt:");
-		System.out.println(Graph.createSubGraph(3, g));
+		AllCyclesInDirectedGraphJohnson johnson = new AllCyclesInDirectedGraphJohnson();
+		List<List<Knoten>> cycles = johnson.simpleCyles(g);
+		System.out.println(cycles.size());
+
+		// System.out.println("SubGraph dargestellt:");
+		// System.out.println(Graph.createSubGraph(3, g));
 	}
 }
