46f1a343939217763461c77c70c186237461f190
JRoemisch <johannes.roemisch@student.fhws.de>
2021-01-15 11:35:59 +0100

    rename "child"->"neighbor"

--- src/zyklusFinder/TarjanStronglyConnectedComponent.java
+++ src/zyklusFinder/TarjanStronglyConnectedComponent.java
@@ -21,7 +21,6 @@ public class TarjanStronglyConnectedComponent {
 
     private Map<Knoten, Integer> indices;
     private Map<Knoten, Integer> lowLink;
-    private Set<Knoten> onStack;
     private Deque<Knoten> stack;
     private Set<Knoten> visited;
     private List<Set<Knoten>> result;
@@ -36,10 +35,6 @@ public class TarjanStronglyConnectedComponent {
 
         //keeps map of node and index of first node visited in current DFS
         lowLink = new HashMap<>();
-
-        //tells if a node is in stack or not
-        // redundant?
-        onStack = new HashSet<>();
 
         //stack of visited nodes
         stack = new LinkedList<>();
@@ -67,38 +62,35 @@ public class TarjanStronglyConnectedComponent {
         lowLink.put(knoten, index);
         index++;
         stack.push(knoten);
-        // stack.addFirst(Knoten);
-        onStack.add(knoten);
 
-        for (Knoten child : knoten.getVerbundeneKnoten()) {
-            //if child is not visited then visit it and see if it has link back to node's ancestor. In that case update
+        for (Knoten neighbor : knoten.getVerbundeneKnoten()) {
+            //if neighbor is not visited then visit it and see if it has link back to node's ancestor. In that case update
             //lowLink to ancestor's index
             // --> DFS until a visited node is hit 
-            if (!visited.contains(child)) {
-                sccUtil(child);
-                //updates lowLink[Knoten] = min(lowLink[Knoten], lowLink[child]);
-                lowLink.put(knoten, Math.min(lowLink.get(knoten), lowLink.get(child)));
-            } //if child is on stack then see if its index is lower than node's lowLink. If yes then update node's lowLink to that.
-            else if (onStack.contains(child)) {
-                //If child is not on stack, then th edge to child is pointing to an SCC already found and must be ignored
+            if (!visited.contains(neighbor)) {
+                sccUtil(neighbor);
+                //updates lowLink[Knoten] = min(lowLink[Knoten], lowLink[neighbor]);
+                lowLink.put(knoten, Math.min(lowLink.get(knoten), lowLink.get(neighbor)));
+            } //if neighbor is on stack then see if its index is lower than node's lowLink. If yes then update node's lowLink to that.
+            else if (stack.contains(neighbor)) {
+                //If neighbor is not on stack, then the edge to neighbor is pointing to an SCC already found and must be ignored
                 //DFS found a leaf/end --> "backtrack" one step and update lowLink value
-                //updates lowLink[Knoten] = min(lowLink[Knoten], indices[child]);
-                lowLink.put(knoten, Math.min(lowLink.get(knoten), indices.get(child)));
+                //updates lowLink[Knoten] = min(lowLink[Knoten], indices[neighbor]);
+                lowLink.put(knoten, Math.min(lowLink.get(knoten), indices.get(neighbor)));
             }
         }
 
         //End of DFS:
         //if nodes lowLink value is the same as index then this is the root node for a strongly connected component.
-        //keep popping node out of stack until you find the current node. They are all part of one strongly connected component.
+        //keep taking nodes off the stack until the current node is found. They are all part of one strongly connected component.
         if (indices.get(knoten) == lowLink.get(knoten)) {
-            Set<Knoten> stronglyConnectedComponenet = new HashSet<>();
+            Set<Knoten> stronglyConnectedComponent = new HashSet<>();
             Knoten v;
             do {
                 v = stack.pollFirst();  // pollFirst() is analogue to pop(), but throws no exception
-                onStack.remove(v);
-                stronglyConnectedComponenet.add(v);
+                stronglyConnectedComponent.add(v);
             } while (!knoten.equals(v));
-            result.add(stronglyConnectedComponenet);
+            result.add(stronglyConnectedComponent);
         }
     }
 }
